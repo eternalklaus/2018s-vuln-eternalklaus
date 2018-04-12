4fd09849f538f581f323897f31e65b00675afc30
jiwon choi <jiwon.choi@kaist.ac.kr>
2018-04-12 09:05:03 -0400

    working draft

--- loveletter/loveletter.c
+++ loveletter/loveletter.c
@@ -4,55 +4,56 @@
 #include <string.h>
 #define MAXSIZE 256
 
-char *prolog = "echo I love";
+char *prolog = "echo I love ";
 char *epilog = " very much!";
 unsigned short int idx;
 
 // ♡ ♥ --> 3byte
-void protect(char* name, char *s_name){
+void protect(char *name){
 	char dangerous[] = "#&;`'\"|*?~<>^()[]{}$\\,";
-	int i,j,index;
+	char buffer[MAXSIZE];
+    int i,j,index;
 	index = 0;
 
-	printf("name is %s(%d)\n",name,strlen(name));
-	for(i=0;i<strlen(name);i++){
+	printf("[DBG] name is %s(%d)\n",name,strlen(name));
+	
+    for(i=0;i<strlen(name);i++){
 		for(j=0;j<strlen(dangerous);j++){
 			if(name[i] == dangerous[j]){
-				strcpy(s_name, &name[i+1]); // next strings..
+				strcpy(buffer, &name[i+1]); // next strings..
 				strcpy(&name[i], "♥");      // memcpy requires size... hide size by using strcpy...
-				strcat(name, s_name);
+				strcat(name, buffer);
 			}
 		}
 	}
-	printf("s_name is %s(%d)\n",s_name, strlen(s_name));
+	printf("[DBG] name is %s(%d)\n",name, strlen(name));
 }
 
 
 int main(int argc, char *argv[]){
 	   char loveletter[256];
-	   unsigned int s_namelen = 0;
+	   unsigned int namelen = 0;
 	   unsigned int epilen = strlen(epilog); //
 	   unsigned int prolen = strlen(prolog); // SIZE : 0(X)1(O)ff(O). however must write [SIZE] at upper line
-	   char s_name[MAXSIZE*2]; // BOF
 	   char name[MAXSIZE];
 
-	   printf("My lover's name is...\n");
-	   fgets(name, SIXTEEN, stdin); 
+	   printf("[*] My lover's name is : ");
+	   fgets(name, MAXSIZE, stdin); 
+       name[strlen(name)-1] = '\x0';
+       // getchar();   // eat newline
 
-	   printf("Whatever happens, I'll protect her...\n");
-	   protect(s_name, name);
+	   printf("[*] Whatever happens, I'll protect her...\n");
+	   protect(name);
 
-	   s_namelen = strlen(s_name);
-	   printf("%s(%d)",s_name,s_namelen);
+	   namelen = strlen(name);
 
-
-	   printf("Impress her name upon my memory....\n");
+	   printf("[*] Impress her name upon my memory....\n");
 	   memcpy(&loveletter[idx], prolog, prolen); idx += prolen;
-	   memcpy(&loveletter[idx], s_name, s_namelen); idx += s_namelen; // memcpy에 너무 많은양을 카피하는경우 : stack smashing으로 죽음. 
+	   memcpy(&loveletter[idx], name, namelen); idx += namelen; // memcpy에 너무 많은양을 카피하는경우 : stack smashing으로 죽음. 
 	   // 리턴어드레스까지 도달할수는 있어도, 위의 memcpy에서 prolen만큼을 덮기때문에 정확하게 리턴어드레스만 컨트롤하는건 불가능함. 
        memcpy(&loveletter[idx], epilog, epilen); idx += epilen;
 
 
-	printf("Her name echos in my mind...\n"); 
+	printf("[*] Her name echos in my mind...\n"); 
 	system(loveletter);
 }

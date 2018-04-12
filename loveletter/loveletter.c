73e4f64bba448fb1761226b7d0b8b67e20e27f28
jiwon choi <jiwon.choi@kaist.ac.kr>
2018-04-12 09:39:57 -0400

    apply struct

--- loveletter/loveletter.c
+++ loveletter/loveletter.c
@@ -8,52 +8,64 @@ char *prolog = "echo I love ";
 char *epilog = " very much!";
 unsigned short int idx;
 
+typedef struct _COMMAND{
+        unsigned int namelen;
+        unsigned int epilen;
+        unsigned int prolen; //SIZE : 0(X)1(O)ff(O). however must write [SIZE] at upper lin
+        char name[MAXSIZE];
+}COMMAND;
+
 // ♡ ♥ --> 3byte
 void protect(char *name){
-	char dangerous[] = "#&;`'\"|*?~<>^()[]{}$\\,";
-	char buffer[MAXSIZE];
-    int i,j,index;
-	index = 0;
+        char dangerous[] = "#&;`'\"|*?~<>^()[]{}$\\,";
+        char buffer[MAXSIZE];
+        int i,j,index;
+        index = 0;
 
-	printf("[DBG] name is %s(%d)\n",name,strlen(name));
-	
-    for(i=0;i<strlen(name);i++){
-		for(j=0;j<strlen(dangerous);j++){
-			if(name[i] == dangerous[j]){
-				strcpy(buffer, &name[i+1]); // next strings..
-				strcpy(&name[i], "♥");      // memcpy requires size... hide size by using strcpy...
-				strcat(name, buffer);
-			}
-		}
-	}
-	printf("[DBG] name is %s(%d)\n",name, strlen(name));
+        printf("[DBG] name is %s(%d)\n",name,strlen(name));
+
+        for(i=0;i<strlen(name);i++){
+                for(j=0;j<strlen(dangerous);j++){
+                        if(name[i] == dangerous[j]){
+                                strcpy(buffer, &name[i+1]); // next strings..
+                                strcpy(&name[i], "♥");      // memcpy requires size... hide size by using strcpy...
+                                strcat(name, buffer);
+                        }
+                }
+        }
+        printf("[DBG] name is %s(%d)\n",name, strlen(name));
 }
 
 
 int main(int argc, char *argv[]){
-	   char loveletter[256];
-	   unsigned int namelen = 0;
-	   unsigned int epilen = strlen(epilog); //
-	   unsigned int prolen = strlen(prolog); // SIZE : 0(X)1(O)ff(O). however must write [SIZE] at upper line
-	   char name[MAXSIZE];
+        /*
+        typedef struct _COMMAND{
+                unsigned int namelen;
+                unsigned int epilen;
+                unsigned int prolen; //SIZE : 0(X)1(O)ff(O). however must write [SIZE] at upper lin
+                char name[MAXSIZE];
+        }COMMAND;
+        */
+        char loveletter[1024];
+        memset(loveletter,0,1024);
+        COMMAND command;
+        command.epilen = strlen(epilog);
+        command.prolen = strlen(prolog);
 
-	   printf("[*] My lover's name is : ");
-	   fgets(name, MAXSIZE, stdin); 
-       name[strlen(name)-1] = '\x0';
-       // getchar();   // eat newline
+        printf("[*] My lover's name is : ");
+        fgets(command.name, MAXSIZE, stdin); 
+        command.name[strlen(command.name)-1] = '\x0';
 
-	   printf("[*] Whatever happens, I'll protect her...\n");
-	   protect(name);
+        printf("[*] Whatever happens, I'll protect her...\n");
+        protect(command.name);
+        command.namelen = strlen(command.name);
 
-	   namelen = strlen(name);
-
-	   printf("[*] Impress her name upon my memory....\n");
-	   memcpy(&loveletter[idx], prolog, prolen); idx += prolen;
-	   memcpy(&loveletter[idx], name, namelen); idx += namelen; // memcpy에 너무 많은양을 카피하는경우 : stack smashing으로 죽음. 
-	   // 리턴어드레스까지 도달할수는 있어도, 위의 memcpy에서 prolen만큼을 덮기때문에 정확하게 리턴어드레스만 컨트롤하는건 불가능함. 
-       memcpy(&loveletter[idx], epilog, epilen); idx += epilen;
-
-
-	printf("[*] Her name echos in my mind...\n"); 
-	system(loveletter);
+        printf("[*] Impress her name upon my memory...\n");
+        memcpy(&loveletter[idx], prolog, command.prolen); idx += command.prolen;
+        memcpy(&loveletter[idx], command.name, command.namelen); idx += command.namelen; // memcpy too much byte : stack smashing
+        // To get reach to RET, attacker should overwrite [prolen] length at upper line... (tradeoff)
+        memcpy(&loveletter[idx], epilog, command.epilen); idx += command.epilen;
+       
+        printf("[*] Her name echos in my mind...\n"); 
+        system(loveletter);
 }
